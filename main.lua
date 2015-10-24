16217e2345fe4f94bef2afccb8dc3bb1d5bf5453
fastline <rapszocrew@gmail.com>
2015-10-25 01:01:12 +0200

    sdasd

--- main.lua
+++ main.lua
@@ -394,7 +394,7 @@ function View:setTermMon()
 end
 
 function View:redirectToTerm()
-	term.redirect(self.termMon.obj)
+	term.redirect(self.termMon.id)
 	self.termMon:reset()
 end
 
@@ -402,7 +402,7 @@ end
 print("Try to gather online devices")
 c = Controller:new()
 v = View:new(_, c:getMonitors())
---print(v.termMon.category)
+print(v.termMon.category)
 v:redirectToTerm()
 --c:regulate()
---print("Valami")
+print("Valami")
