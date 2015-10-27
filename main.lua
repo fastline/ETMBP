0e3833107e4f589320a01e2ef42e2c20b7d96166
fastline <rapszocrew@gmail.com>
2015-10-27 20:42:58 +0100

    dfdfdfdfdfd

--- main.lua
+++ main.lua
@@ -250,9 +250,6 @@ function Controller:wrapAll()
 	controlledDevices = {}
 	for i, v in pairs(devicesList) do
 		--print("0 ", tostring(#controlledDevices))
-		if ((#controlledDevices) > 1 ) then
-			print("elotte: ",tostring(controlledDevices[#controlledDevices - 1 ].id))
-		end
 		if string.find(v, "-Reactor") then
 			dObj = Reactor:new(nil, v)
 			table.insert(controlledDevices, dObj)
@@ -277,6 +274,9 @@ function Controller:wrapAll()
 		--print("1 ", tostring(#controlledDevices))
 		--print("ID: ", controlledDevices[#controlledDevices].id)
 		--print("Cat:"..controlledDevices[#controlledDevices].category)
+		if ((#controlledDevices) > 1 ) then
+			print("elotte: ",tostring(controlledDevices[#controlledDevices - 1 ].id))
+		end
 	end
 	--debugTable(controlledDevices)
 	print(tostring(controlledDevices[2].id))
